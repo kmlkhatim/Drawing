16e0baa2fbcec1d6baf9ca5be21a080e488c8e06
kmlkhatim <kmlkhatim@gmail.com>
2023-04-19 23:33:48 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Kamal KHATIM on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
