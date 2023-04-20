fee7ffe69f9289a7753cb8a617efce87c3dd483b
kmlkhatim <kmlkhatim@gmail.com>
2023-04-20 02:29:38 +0100

    testing

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+struct Arc: InsettableShape {
+    
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insertAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let rotatioAdjustement = Angle.degrees(90)
+        let modifiedStart = startAngle - rotatioAdjustement
+        let modifiedEnd = endAngle - rotatioAdjustement
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        
+        var arc = self
+        arc.insertAmount += amount
+        
+        return arc
+    }
+    
+    
+}
+
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
     }
 }
 
